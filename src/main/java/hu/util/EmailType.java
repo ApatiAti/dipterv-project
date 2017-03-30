97060a5b23b98e980dc377d84a1b43fb5f354229
ApatiAti <tntiti@hotmail.com>
2017-03-30 23:19:31 +0200

    New Email
    ConsultationHour módosítását jelző levél létrehozása.

--- src/main/java/hu/util/EmailType.java
+++ src/main/java/hu/util/EmailType.java
@@ -2,7 +2,9 @@ package hu.util;
 
 public enum EmailType {
 	TESZT_EMAIL("NewFile", "Teszteléshez tökéletes"),
-	DOCUMENT_UPLOADED("newDocumentUploaded", "Feltöltésre került egy új dokumentum.");
+	DOCUMENT_UPLOADED("newDocumentUploaded", "Feltöltésre került egy új dokumentum."),
+	CONSULTATION_HOUR_MODIFIED("consultationHourModified", "Foglalt időpontja megváltozott"),
+	;
 	
 	private final String emailTemplateName;
 	private final String emailSubject;
