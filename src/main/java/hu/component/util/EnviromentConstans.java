c75aa2425fd09b2b09092fd10a6d0ad35f695a73
ApatiAti <tntiti@hotmail.com>
2017-04-17 05:00:25 +0200

    MailService konfigurálásának módosítása

---
+++ src/main/java/hu/component/util/EnviromentConstans.java
@@ -0,0 +1,14 @@
+package hu.component.util;
+
+public class EnviromentConstans {
+
+	public static final String IS_TEST_SERVER = "server.istest";
+	
+	/*
+	 * MailService-hez kapcsolódó config paraméterek
+	 */
+	public static final String MAIL_SERVIC_DISABLED = "mail.service.disabled";
+	public static final String MAIL_SERVIC_DEFAULT_EMAIL = "mail.service.default.email";
+	
+	
+}
