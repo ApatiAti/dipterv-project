11f00b84867e9a2804756f80f5db68b206b42228
ApatiAti <tntiti@hotmail.com>
2017-04-20 18:25:01 +0200

    Swagger által generált API hozzáadása

---
+++ src/main/java/io/swagger/model/ConsultationHourSearch.java
@@ -0,0 +1,144 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.joda.time.DateTime;
+
+
+
+
+/**
+ * ConsultationHourSearch
+ */
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-04-20T03:07:35.390+02:00")
+
+public class ConsultationHourSearch   {
+  private String departmentName = null;
+
+  private Long type = null;
+
+  private DateTime beginDate = null;
+
+  private DateTime endDate = null;
+
+  public ConsultationHourSearch departmentName(String departmentName) {
+    this.departmentName = departmentName;
+    return this;
+  }
+
+   /**
+   * Department's name
+   * @return departmentName
+  **/
+  @ApiModelProperty(value = "Department's name")
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+  public ConsultationHourSearch type(Long type) {
+    this.type = type;
+    return this;
+  }
+
+   /**
+   * Type of the consultationHour
+   * @return type
+  **/
+  @ApiModelProperty(value = "Type of the consultationHour")
+  public Long getType() {
+    return type;
+  }
+
+  public void setType(Long type) {
+    this.type = type;
+  }
+
+  public ConsultationHourSearch beginDate(DateTime beginDate) {
+    this.beginDate = beginDate;
+    return this;
+  }
+
+   /**
+   * Start date of the ConsultationHour
+   * @return beginDate
+  **/
+  @ApiModelProperty(value = "Start date of the ConsultationHour")
+  public DateTime getBeginDate() {
+    return beginDate;
+  }
+
+  public void setBeginDate(DateTime beginDate) {
+    this.beginDate = beginDate;
+  }
+
+  public ConsultationHourSearch endDate(DateTime endDate) {
+    this.endDate = endDate;
+    return this;
+  }
+
+   /**
+   * End date of the ConsultationHour
+   * @return endDate
+  **/
+  @ApiModelProperty(value = "End date of the ConsultationHour")
+  public DateTime getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(DateTime endDate) {
+    this.endDate = endDate;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ConsultationHourSearch consultationHourSearch = (ConsultationHourSearch) o;
+    return Objects.equals(this.departmentName, consultationHourSearch.departmentName) &&
+        Objects.equals(this.type, consultationHourSearch.type) &&
+        Objects.equals(this.beginDate, consultationHourSearch.beginDate) &&
+        Objects.equals(this.endDate, consultationHourSearch.endDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(departmentName, type, beginDate, endDate);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ConsultationHourSearch {\n");
+    
+    sb.append("    departmentName: ").append(toIndentedString(departmentName)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    beginDate: ").append(toIndentedString(beginDate)).append("\n");
+    sb.append("    endDate: ").append(toIndentedString(endDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ src/main/java/io/swagger/model/Department.java
@@ -0,0 +1,129 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.ConsultationHourType;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+/**
+ * Department
+ */
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-04-20T03:07:35.390+02:00")
+
+public class Department   {
+  private Long departmentId = null;
+
+  private String departmentName = null;
+
+  private List<ConsultationHourType> consultationHourType = new ArrayList<ConsultationHourType>();
+
+  public Department departmentId(Long departmentId) {
+    this.departmentId = departmentId;
+    return this;
+  }
+
+   /**
+   * Id of department
+   * @return departmentId
+  **/
+  @ApiModelProperty(value = "Id of department")
+  public Long getDepartmentId() {
+    return departmentId;
+  }
+
+  public void setDepartmentId(Long departmentId) {
+    this.departmentId = departmentId;
+  }
+
+  public Department departmentName(String departmentName) {
+    this.departmentName = departmentName;
+    return this;
+  }
+
+   /**
+   * Name of the department
+   * @return departmentName
+  **/
+  @ApiModelProperty(value = "Name of the department")
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+  public Department consultationHourType(List<ConsultationHourType> consultationHourType) {
+    this.consultationHourType = consultationHourType;
+    return this;
+  }
+
+  public Department addConsultationHourTypeItem(ConsultationHourType consultationHourTypeItem) {
+    this.consultationHourType.add(consultationHourTypeItem);
+    return this;
+  }
+
+   /**
+   * Get consultationHourType
+   * @return consultationHourType
+  **/
+  @ApiModelProperty(value = "")
+  public List<ConsultationHourType> getConsultationHourType() {
+    return consultationHourType;
+  }
+
+  public void setConsultationHourType(List<ConsultationHourType> consultationHourType) {
+    this.consultationHourType = consultationHourType;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Department department = (Department) o;
+    return Objects.equals(this.departmentId, department.departmentId) &&
+        Objects.equals(this.departmentName, department.departmentName) &&
+        Objects.equals(this.consultationHourType, department.consultationHourType);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(departmentId, departmentName, consultationHourType);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Department {\n");
+    
+    sb.append("    departmentId: ").append(toIndentedString(departmentId)).append("\n");
+    sb.append("    departmentName: ").append(toIndentedString(departmentName)).append("\n");
+    sb.append("    consultationHourType: ").append(toIndentedString(consultationHourType)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
