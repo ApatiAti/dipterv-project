d29638254b26f2f0855b65f1f12555f667405fe5
ApatiAti <tntiti@hotmail.com>
2017-04-29 14:15:58 +0200

    Modifying APi request URLs

--- src/main/java/io/swagger/api/ConsultationHourSearchApi.java
+++ src/main/java/io/swagger/api/ConsultationHourSearchApi.java
@@ -1,22 +1,18 @@
 package io.swagger.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.model.ConsultationHour;
 import io.swagger.model.ConsultationHourSearch;
 import io.swagger.model.Error;
-
-import io.swagger.annotations.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-04-29T11:37:00.219Z")
 
 @Api(value = "consultationHourSearch", description = "the consultationHourSearch API")
@@ -26,7 +22,7 @@ public interface ConsultationHourSearchApi {
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "An array of products", response = ConsultationHour.class),
         @ApiResponse(code = 400, message = "Unexpected error", response = Error.class) })
-    @RequestMapping(value = "/consultationHourSearch",
+    @RequestMapping(value = "/api/consultationHourSearch",
         produces = { "application/json" }, 
         method = RequestMethod.POST)
     ResponseEntity<Object> consultationHourSearchPost(@ApiParam(value = "Search data" ,required=true ) @RequestBody ConsultationHourSearch request);
