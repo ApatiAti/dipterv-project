e54bd022963cef1b450006d079ce1f4f150ba12a
ApatiAti <tntiti@hotmail.com>
2017-04-30 18:40:47 +0200

    Fixing cherry-pick problems

--- src/main/resources/scripts/no_table_test.sql
+++ src/main/resources/scripts/no_table_test.sql
@@ -1,12 +1,9 @@
 ﻿ --
 -- Table structure for table user
 --
-<<<<<<< HEAD:src/main/resources/scripts/no_table_test.sql
-ALTER TABLE user ADD password VARCHAR(255);
-=======
+
 ALTER TABLE users ADD password VARCHAR(255);
 ALTER TABLE user ADD COLUMN enabled SMALLINT(4) NULL DEFAULT '1';
->>>>>>> 5387da7... Fixing errors:src/main/resources/scripts/insert.sql
 --
 -- Dumping data for table user
 --
