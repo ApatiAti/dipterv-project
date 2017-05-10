45f46058e399a06b5624360cfcb4f2947225f145
ApatiAti <tntiti@hotmail.com>
2017-05-10 20:28:05 +0200

    Add commnet

--- src/test/java/hu/config/MyTestClassListener.java
+++ src/test/java/hu/config/MyTestClassListener.java
@@ -1,5 +1,11 @@
 package hu.config;
 
+/**
+ * Ennek és a MySpringJUnit4ClassRunner használatával elérhető hogy ne csak static
+ * tipusú metódus lehessen csak @BeforeClass vagy @AfterClass tulajdonságokkal felruházva.
+ * @author Apati
+ *
+ */
 public interface MyTestClassListener {
     void beforeClassSetup();
     void afterClassSetup();
